f66d9d22602c3f5780322bfe3bc9af79adbb459b
student <student05@itpark.ru>
2018-12-01 12:19:28 +0300

    test cashback above limit

--- app/test/test_cashback.py
+++ app/test/test_cashback.py
@@ -5,3 +5,8 @@ def test_cashback_under_limit ():
     result = cashback(1000)
 
     assert 50 == result
+
+def test_cashback_above_limit():
+    result = cashback(1_000_000)
+
+    assert 3_000 == result
